80cc0a4080dfdf41cf9e1814b8f152adcfdac091
Andrei Chaika <redsn@inbox.ru>
2022-05-11 05:57:10 +0300

    feat: section3

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ alert(
 
 // В body размещаю базовый div с class="wrapper"
 const qBody = document.querySelector('body');
-const wrapper = document.createElement('div');
-wrapper.classList.add('wrapper');
+const keyboard = document.createElement('div');
+keyboard.classList.add('wrapper');
 document.body.appendChild(keyboard);
 
 // Добавляю section с class="header" и id="header"
 const section1 = document.createElement('section');
 section1.classList.add('header');
 section1.id = 'header';
-wrapper.appendChild(section1);
+keyboard.appendChild(section1);
 
 // Добавляю заголовок h1 с class="title" с текстом RSS Виртуальная клавиатура
 const h1 = document.createElement('h1');
@@ -29,9 +29,14 @@ section2.id = 'input';
 const textarea = document.createElement('textarea');
 textarea.classList.add('input-area');
 section2.appendChild(textarea);
-wrapper.appendChild(section2);
+keyboard.appendChild(section2);
 
 // Добавляю ещё одну section с class="keyboard" и id="keyboard"
 const section3 = document.createElement('section');
 section3.classList.add('keyboard');
 section3.id = 'keyboard';
+
+const div1 = document.createElement('div');
+div1.classList.add('keyboard-body');
+
+section3.appendChild(div1);
