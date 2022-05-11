006060fb32062dc5f34727671ff5fa66deca7f4d
Andrei Chaika <redsn@inbox.ru>
2022-05-11 03:35:47 +0300

    fix: edit base

---
+++ enkeys.js
@@ -0,0 +1,59 @@
+const enLang = [
+    [
+        { name: "Key_Backtick", key: "`", shiftKey: "~" },
+        { name: "Key_1", key: "1", shiftKey: "!" },
+        { name: "Key_2", key: "2", shiftKey: "@" },
+        { name: "Key_3", key: "3", shiftKey: "#" },
+        { name: "Key_4", key: "4", shiftKey: "$" },
+        { name: "Key_5", key: "5", shiftKey: "%" },
+        { name: "Key_6", key: "6", shiftKey: "6" },
+        { name: "Key_7", key: "7", shiftKey: "&" },
+        { name: "Key_8", key: "8", shiftKey: "*" },
+        { name: "Key_9", key: "9", shiftKey: "(" },
+        { name: "Key_0", key: "0", shiftKey: ")" },
+        { name: "Key_Minus", key: "-", shiftKey: "_" },
+        { name: "Key_Equal", key: "=", shiftKey: "+" }
+    ],
+    [
+        { name: "Key_Q", key: "q", shiftKey: "Q" },
+        { name: "Key_W", key: "w", shiftKey: "W" },
+        { name: "Key_E", key: "e", shiftKey: "E" },
+        { name: "Key_R", key: "r", shiftKey: "R" },
+        { name: "Key_T", key: "t", shiftKey: "T" },
+        { name: "Key_Y", key: "y", shiftKey: "Y" },
+        { name: "Key_U", key: "u", shiftKey: "U" },
+        { name: "Key_I", key: "i", shiftKey: "I" },
+        { name: "Key_O", key: "o", shiftKey: "O" },
+        { name: "Key_P", key: "p", shiftKey: "P" },
+        { name: "Key_Bracket_Left", key: "[", shiftKey: "]" },
+        { name: "Key_Bracket_Right", key: "{", shiftKey: "}" },
+        { name: "Key_Backslash", key: "\\", shiftKey: "|" }     
+    ],
+    [
+        { name: "Key_A", key: "a", shiftKey: "A" },
+        { name: "Key_S", key: "s", shiftKey: "S" },
+        { name: "Key_D", key: "d", shiftKey: "D" },
+        { name: "Key_F", key: "f", shiftKey: "F" },
+        { name: "Key_G", key: "g", shiftKey: "G" },
+        { name: "Key_H", key: "h", shiftKey: "H" },
+        { name: "Key_J", key: "j", shiftKey: "J" },
+        { name: "Key_K", key: "k", shiftKey: "K" },
+        { name: "Key_L", key: "l", shiftKey: "L" },
+        { name: "Key_Semicolon", key: ";", shiftKey: ":" },
+        { name: "Key_Quote", key: "\'", shiftKey: "\"" }
+    ],
+    [
+        { name: "Key_Z", key: "z", shiftKey: "Z" },
+        { name: "Key_X", key: "x", shiftKey: "X" },
+        { name: "Key_C", key: "c", shiftKey: "C" },
+        { name: "Key_V", key: "v", shiftKey: "V" },
+        { name: "Key_B", key: "b", shiftKey: "B" },
+        { name: "Key_N", key: "n", shiftKey: "N" },
+        { name: "Key_M", key: "m", shiftKey: "M" },
+        { name: "Key_Comma", key: ",", shiftKey: "<" },
+        { name: "Key_Dot", key: ".", shiftKey: ">" },
+        { name: "Key_Slash", key: "/", shiftKey: "?" },     
+    ],
+];
+
+export default enLang;
---
+++ rukeys.js
@@ -0,0 +1,59 @@
+const ruLang = [
+    [
+        { name: "Key_Backtick", key: "ё", shiftKey: "Ё" },
+        { name: "Key_1", key: "1", shiftKey: "!" },
+        { name: "Key_2", key: "2", shiftKey: "\"" },
+        { name: "Key_3", key: "3", shiftKey: "№" },
+        { name: "Key_4", key: "4", shiftKey: ";" },
+        { name: "Key_5", key: "5", shiftKey: "%" },
+        { name: "Key_6", key: "6", shiftKey: ":" },
+        { name: "Key_7", key: "7", shiftKey: "?" },
+        { name: "Key_8", key: "8", shiftKey: "*" },
+        { name: "Key_9", key: "9", shiftKey: "(" },
+        { name: "Key_0", key: "0", shiftKey: ")" },
+        { name: "Key_Minus", key: "-", shiftKey: "_" },
+        { name: "Key_Equal", key: "=", shiftKey: "+" }
+    ],
+    [
+        { name: "Key_Q", key: "й", shiftKey: "Й" },
+        { name: "Key_W", key: "ц", shiftKey: "Ц" },
+        { name: "Key_E", key: "у", shiftKey: "У" },
+        { name: "Key_R", key: "к", shiftKey: "К" },
+        { name: "Key_T", key: "е", shiftKey: "Е" },
+        { name: "Key_Y", key: "н", shiftKey: "Н" },
+        { name: "Key_U", key: "г", shiftKey: "Г" },
+        { name: "Key_I", key: "ш", shiftKey: "Ш" },
+        { name: "Key_O", key: "щ", shiftKey: "Щ" },
+        { name: "Key_P", key: "з", shiftKey: "З" },
+        { name: "Key_Bracket_Left", key: "х", shiftKey: "Х" },
+        { name: "Key_Bracket_Right", key: "ъ", shiftKey: "Ъ" },
+        { name: "Key_Backslash", key: "\\", shiftKey: "/" }     
+    ],
+    [
+        { name: "Key_A", key: "ф", shiftKey: "Ф" },
+        { name: "Key_S", key: "ы", shiftKey: "Ы" },
+        { name: "Key_D", key: "в", shiftKey: "В" },
+        { name: "Key_F", key: "а", shiftKey: "А" },
+        { name: "Key_G", key: "п", shiftKey: "П" },
+        { name: "Key_H", key: "р", shiftKey: "Р" },
+        { name: "Key_J", key: "о", shiftKey: "О" },
+        { name: "Key_K", key: "л", shiftKey: "Л" },
+        { name: "Key_L", key: "д", shiftKey: "Д" },
+        { name: "Key_Semicolon", key: "ж", shiftKey: "Ж" },
+        { name: "Key_Quote", key: "э", shiftKey: "Э" }
+    ],
+    [
+        { name: "Key_Z", key: "я", shiftKey: "Я" },
+        { name: "Key_X", key: "ч", shiftKey: "Ч" },
+        { name: "Key_C", key: "с", shiftKey: "С" },
+        { name: "Key_V", key: "м", shiftKey: "М" },
+        { name: "Key_B", key: "и", shiftKey: "И" },
+        { name: "Key_N", key: "т", shiftKey: "Т" },
+        { name: "Key_M", key: "ь", shiftKey: "Ь" },
+        { name: "Key_Comma", key: "б", shiftKey: "Б" },
+        { name: "Key_Dot", key: "ю", shiftKey: "Ю" },
+        { name: "Key_Slash", key: ".", shiftKey: "," },     
+    ],
+];
+
+export default ruLang;
