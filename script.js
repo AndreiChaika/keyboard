f7a7dc8b48753216c55e3f09e3ced17ffeb2f49b
Andrei Chaika <redsn@inbox.ru>
2022-05-11 07:04:27 +0300

    feat: keys

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ document.body.appendChild(main);
 const footer = document.createElement('footer');
 document.body.appendChild(footer);
 
-
 //
 // Контент для header
 //
@@ -28,8 +27,6 @@ const h1 = document.createElement('h1');
 header.appendChild(h1);
 h1.textContent = 'Virtual Keyboard by Andrei Chaika';
 
-
-
 //
 // Контент для footer
 //
@@ -38,3 +35,32 @@ h1.textContent = 'Virtual Keyboard by Andrei Chaika';
 const p = document.createElement('p');
 footer.appendChild(p);
 p.textContent = 'Клавиатура создана для ОС Windows. Переключение языка осуществляется через комбинацию Alt + Shift';
+
+
+
+
+
+// Теперь начинается основа разработки 
+
+//
+// Структура для main
+//
+
+// Добавляю секцию с классом "input", где будет размещаться поле для вывода текста с клавиатуры
+const input = document.createElement('section');
+input.classList.add('input');
+main.appendChild(input);
+// Размещаю поле для вывода текста с клавиатуры
+const textarea = document.createElement('textarea');
+textarea.classList.add('textarea');
+input.appendChild(textarea);
+
+// Добавляю секцию с классом "keyboard", где будет размещаться сама виртуальная клавиатура
+const keyboard = document.createElement('section');
+keyboard.classList.add('keyboard');
+input.appendChild(keyboard);
+
+// Добавляю внутрь этой секции блок в котором будут клавиши клавиатуры
+const keys = document.createElement('div');
+keys.classList.add('keys');
+keyboard.appendChild(keys);
