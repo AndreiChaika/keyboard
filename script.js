d9d181cb7d3f4caeb281487a8f4fe5436af435e7
Andrei Chaika <redsn@inbox.ru>
2022-05-11 06:25:39 +0300

    feat: header and main

--- script.js
+++ script.js
@@ -2,48 +2,11 @@ alert(
   "Привет. Не успел доделать. Буду благодарен, если повременишь с проверкой. Спасибо за понимание :)"
 );
 
-// В body размещаю базовый div с class="wrapper"
-const qBody = document.querySelector('body');
-const keyboard = document.createElement('div');
-keyboard.classList.add('wrapper');
-document.body.appendChild(keyboard);
+// Добавляю header, где размещу информацию о названии и как переключать язык с помощью клавиатуры
+const header = document.createElement('header');
+document.body.appendChild(header);
 
-// Добавляю section с class="header" и id="header"
-const section1 = document.createElement('section');
-section1.classList.add('header');
-section1.id = 'header';
-keyboard.appendChild(section1);
+// Создаю main и размещаю main
+const main = document.createElement('main');
+document.body.appendChild(main);
 
-// Добавляю заголовок h1 с class="title" с текстом RSS Виртуальная клавиатура
-const h1 = document.createElement('h1');
-h1.classList.add('title');
-section1.appendChild(h1);
-h1.textContent = 'Virtual Keyboard by Andrei Chaika';
-
-// Добавляю ещё одну section с class="input" и id="input"
-const section2 = document.createElement('section');
-section2.classList.add('input');
-section2.id = 'input';
-
-// Добавляю поле для ввода текста textarea с классом input-area. Далее применяю его в секцию и базовый div
-const textarea = document.createElement('textarea');
-textarea.classList.add('input-area');
-section2.appendChild(textarea);
-keyboard.appendChild(section2);
-
-// Добавляю ещё одну section с class="keyboard" и id="keyboard"
-const section3 = document.createElement('section');
-section3.classList.add('keyboard');
-section3.id = 'keyboard';
-
-keyboard.appendChild(section3);
-
-const div1 = document.createElement('div');
-div1.classList.add('keyboard-body');
-
-section3.appendChild(div1);
-
-const div2 = document.createElement('div');
-div2.classList.add('keyboard-buttons');
-
-div1.appendChild(div2);
