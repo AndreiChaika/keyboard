dd91cb518a2a99b2598db84662bc0d488fb7a2e2
Andrei Chaika <redsn@inbox.ru>
2022-05-11 04:23:45 +0300

    fix: newbody

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ alert(
   "Привет. Не успел доделать. Буду благодарен, если повременишь с проверкой. Спасибо за понимание :)"
 );
 
-let qBody = document.querySelector(".body");
-console.log(qBody);
+let newBody = document.querySelector(".body");
+console.log(newBody);
