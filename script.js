8092c55cd797622aadec7268c9932eb964ed7bf5
Andrei Chaika <redsn@inbox.ru>
2022-05-11 05:22:52 +0300

    feat: new sections and divs

--- script.js
+++ script.js
@@ -2,94 +2,36 @@ alert(
   "Привет. Не успел доделать. Буду благодарен, если повременишь с проверкой. Спасибо за понимание :)"
 );
 
-let qBody = document.querySelector("body");
-console.log(qBody);
-
-const keyboard = document.createElement("section");
+// В body размещаю базовый div с class="wrapper"
+const qBody = document.querySelector('body');
+const keyboard = document.createElement('div');
 keyboard.classList.add('wrapper');
-keyboard.innerHTML = `
-<div class="row">
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-</div>
-<div class="row">
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-</div>
-<div class="row">
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-</div>
-<div class="row">
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-</div>
-<div class="row">
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-    <div class="key"></div>
-</div>
-`;
-
-
-
 document.body.appendChild(keyboard);
+
+// Добавляю section с class="header" и id="header"
+const section1 = document.createElement('section');
+section1.classList.add('header');
+section1.id = 'header';
+wrapper.appendChild(section1);
+
+// Добавляю заголовок h1 с class="title" с текстом RSS Виртуальная клавиатура
+const h1 = document.createElement('h1');
+h1.classList.add('title');
+section1.appendChild(h1);
+h1.textContent = 'Virtual Keyboard by Andrei Chaika';
+
+// Добавляю ещё одну section с class="input" и id="input"
+const section2 = document.createElement('section');
+section2.classList.add('input');
+section2.id = 'input';
+
+// Добавляю поле для ввода текста textarea с классом input-area. Далее применяю его в секцию и базовый div
+const textarea = document.createElement('textarea');
+textarea.classList.add('input-area');
+section2.appendChild(textarea);
+wrapper.appendChild(section2);
+
+// Добавляю ещё одну section с class="keyboard" и id="keyboard"
+const section3 = document.createElement('section');
+section3.classList.add('keyboard');
+section3.id = 'keyboard';
