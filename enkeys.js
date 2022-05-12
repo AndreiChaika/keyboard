126fc9be45cc75200d48faffd97809d0619c80f5
Andrei Chaika <redsn@inbox.ru>
2022-05-12 05:09:36 +0300

    fix: enkeys.js

--- enkeys.js
+++ enkeys.js
@@ -1,5 +1,5 @@
 const enLang = [
-    [{key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', keyCode: 17, code: 'ControlLeft', location: 1, altKey: true, ctrlKey: false, metaKey: true, shiftKey: true, description: 'ctrl', unicode: '^', class: 'size2 dark', property: 'nonprintable', }, ], 
+    [{key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', code: 'ControlLeft', }, ], 
 ];
  
 
