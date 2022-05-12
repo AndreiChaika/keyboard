67a38e3f3d3fade2cf5f2128aaddba8c32659312
Andrei Chaika <redsn@inbox.ru>
2022-05-12 17:50:38 +0300

    fix: architecture

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ alert(
 import enLang from './enkeys.js';
 import ruLang from './rukeys.js';
 
+let keys = enLang;
 // 
 // Для начала задам структуру header, main, footer 
 //
@@ -87,65 +88,48 @@ for (let i = 0; i < keys.length; i += 1) {
 }
 
 
-const virtKeyboard = document.querySelector('.keyboard');
-const virtBackspace = document.querySelector('#Key_Backspace');
-const virtEnter = document.querySelector('#Key_Enter');
-const virtTab = document.querySelector('#Key_Tab');
-const virtDel = document.querySelector('#Key_Delete');
-const virtShiftL = document.querySelector('#Key_Shift_Left');
-const virtShiftR = document.querySelector('#Key_Shift_Right');
-const virtCtrlL = document.querySelector('#Key_Ctrl_Left');
-const virtCtrlR = document.querySelector('#Key_Ctrl_Right');
-const virtAltL = document.querySelector('#Key_Alt_Left');
-const virtAltR = document.querySelector('#Key_Alt_Right');
-const virtSpace = document.querySelector('#Key_Space');
+const inputText = document.querySelector('textarea');
+const keyboard = document.querySelector('#keyboard');
+const btnEnter = document.querySelector('#Enter');
+const btnTab = document.querySelector('#Tab');
+const btnDel = document.querySelector('#Delete');
+const btnBackspace = document.querySelector('#Backspace');
+const btnCapsLock = document.querySelector('#CapsLock');
+const btnShiftL = document.querySelector('#ShiftLeft');
+const btnShiftR = document.querySelector('#ShiftRight');
+const btnCtrlL = document.querySelector('#ControlLeft');
+const btnCtrlR = document.querySelector('#ControlRight');
+const btnAltL = document.querySelector('#AltLeft');
+const btnAltR = document.querySelector('#AltRight');
+const btnSpace = document.querySelector('#Space');
+const btnSymbols = document.querySelectorAll('button[printable]');
 
-//
-// Сейчас выводятся в клавиши только значения ключа key из массива enkeys.js. 
-// Нужно задать смену этих значений при зажатии клавиши Shift и при смене языка Alt+Shift  
-//
+let lang = 'en';
+let shiftState = 0;
+let altState = 0;
 
-// Для начала выведем в отдельный массив те клавиши, которые подвержены изменению с классом shortkey
-const virtSymbols = document.querySelectorAll('button.shortkey');
-
-// Добавим переменные ShiftStatus означающий нажатие кнопки Shift, а также altStatus означающий нажатие кнопки Alt
-let shiftStatus = 0;
-let altStatus = 0;
-
-// Нужна функция по совпадению использованной клавиши с тем, что реализовано в массиве
 function findKey(code) {
-  // Для начала создаю цикл, который сперва будет перебирать массив по элементам (оно же i)
   for (let i = 0; i < keys.length; i += 1) {
-    //  Внутрь цикла размещаю ещё один цикл, который внутри i-ых элементов массива будет перебирать элементы вложенных в них массивов (они же j) 
     for (let j = 0; j < keys[i].length; j += 1) {
-      // Добавляю условие для перебора всего массива на совпадение, что если значение ключа code из массива равно тому значению, что использовано, то возвращать его данные 
       if (keys[i][j].code === code) return keys[i][j];
     }
   }
-  // Если нет, то возвращать null;
   return null;
 }
 
-// Создаём функцию по смене языка клавиш
-function translate() {
-  // Для начала для каждого элемента массива обычных клавиш с классом shortkey применю функцию 
-  virtSymbols.forEach((el) => {
-    // Задам переменную по вводимому элементу
-    const virtElement = el;
-    // К функции. Если Shift не нажат, то используется обычная клавиша Key
-    if (shiftStatus === 0) {
-      // И на место элемента вставляется тот code, что был найден в предыдущей функции по поиску совпадения
-      virtElement.innerHTML = findKey(virtElement.id).name;
+function changeLetters() {
+  btnSymbols.forEach((el) => {
+    const elt = el;
+    if (shiftState === 0) {
+      elt.innerHTML = findKey(elt.id).name;
     } else {
-      // Иначе на место элемента подставляется из массива значение шифтовой клавиши shiftKey
-      virtElement.innerHTML = findKey(virtElement.id).shiftName;
+      elt.innerHTML = findKey(elt.id).nameShift;
     }
   });
 }
 
-// Добавляю функцию с switch (сравнивает выражение с случаем, перечисленным внутри него)
-function setLanguage(language) {
-  switch (language) {
+function setLang(langT) {
+  switch (langT) {
     case 'en':
       keys = enLang;
       break;
@@ -156,132 +140,92 @@ function setLanguage(language) {
     default:
       break;
   }
-  translate();
+  changeLetters();
 }
 
-/*
-// Просто фича, которую можно сделать для удобства
-// Создадим переменную с произвольным значением 'en'
-let lang = 'en';
-
-// Можно сделать сохранение выбранного языка в веб-хранилище localStorage 
-// он позволяет сохранить ключ lang с значением в виде переменной lang в браузере 
-// после обновления страницы и даже перезапуска браузера 
 function setLocalStorage() {
   localStorage.setItem('lang', lang);
 }
-
-// Если пользователь покидает страницу (событие beforeunload), то сохраняем значения за счёт этой функции
 window.addEventListener('beforeunload', setLocalStorage);
 
-// Когда пользователь вернётся, то мы подгружаем выбранный язык
 function getLocalStorage() {
-  // Передавая ключ lang в качестве параметра, то метод вернёт значение, лежащее в хранилище localStorage по указанному ключу.
   if (localStorage.getItem('lang')) {
     lang = localStorage.getItem('lang');
-    setLanguage(lang);
+    setLang(lang);
   }
 }
 window.addEventListener('load', getLocalStorage);
 
-*/
-// Зададим переменную по полю для ввода текста
-const virtTextarea = document.querySelector('textarea');
-
-// Зададим функцию позиционирования
-function cursorPosition() {
-  // Пускай переменная обозначающая позицию по-умолчанию будет 0
+function getCursorPosition() {
   let position = 0;
-  // Если позиция начала выделения элемента textarea не null и не undefined, то возвращаем его значение 
-  if ((virtTextarea.selectionStart != null) && (virtTextarea.selectionStart !== undefined)) {
-    position = virtTextarea.selectionStart;
+  if ((inputText.selectionStart != null) && (inputText.selectionStart !== undefined)) {
+    position = inputText.selectionStart;
   }
   return position;
 }
-// Фича про выделение текста
-// Добавляю функцию вставить символы в textarea
+
 function insertSymbols(str) {
-  const position = cursorPosition();
-  // Левая часть - возврат подстроки между индексом 0 и значением position из ранее описанной функции cursorPosition()
-  const leftPart = virtTextarea.value.substring(0, position);
-  // Правая часть - возврат подстроки по индексу значения position из ранее описанной функции cursorPosition()
-  const rightPart = virtTextarea.value.substring(position);
-  // Для удобства добавлю переменную выражающую длину строки str
+  const position = getCursorPosition();
+  const leftPart = inputText.value.substring(0, position);
+  const rightPart = inputText.value.substring(position);
   let strLen = str.length;
-  // Если str равен переносу строки, то длина будет равна 1
   if (str === '\r\n') strLen = 1;
-  // Атрибут value
-  virtTextarea.value = leftPart + str + rightPart;
-  // Сфокусированный элемент — по умолчанию будет получать события клавиатуры и подобные события.
-  virtTextarea.focus();
-  // Устанавим начальное и конечное положение выделения текста в элементе <textarea>
-  virtTextarea.setSelectionRange(position + strLen, position + strLen);
+  inputText.value = leftPart + str + rightPart;
+  inputText.focus();
+  inputText.setSelectionRange(position + strLen, position + strLen);
 }
 
-
-// Создаём функцию, которая определяет нажатую кнопку
 function btnsClicked(event) {
-  // Определяем переменную которая будет равняться элементу в котором происходит событие
   const { id } = event.currentTarget;
   if (findKey(id).location > 0) return;
   if (id === 'Enter' || id === 'Tab' || id === 'Space') return;
   insertSymbols(event.currentTarget.innerHTML);
 }
 
-// Функция нажатия на enter подразумивает добавление символов \r\n в textarea
 function enterClicked() {
   insertSymbols('\r\n');
 }
 
-// Функция нажатия на tab подразумивает добавление      в textarea
 function tabClicked() {
   insertSymbols('    ');
 }
 
-// Функция нажатия на space подразумивает добавление   в textarea
 function spaceClicked() {
   insertSymbols(' ');
 }
 
-// Функция нажатия на delete в textarea
 function delClicked() {
-  // определяем текущую позицию курсора (мигающего знака строки между символами)
-  const position = cursorPosition();
-  virtTextarea.value = virtTextarea.value.substring(0, position)
-    + virtTextarea.value.substring(position + 1);
-    virtTextarea.focus();
-    virtTextarea.setSelectionRange(position, position);
+  const position = getCursorPosition();
+  inputText.value = inputText.value.substring(0, position)
+    + inputText.value.substring(position + 1);
+  inputText.focus();
+  inputText.setSelectionRange(position, position);
 }
 
-
-// Функция нажатия на backspace в textarea
 function backspaceClicked() {
-  const position = cursorPosition();
+  const position = getCursorPosition();
   if (position > 0) {
-    virtTextarea.value = virtTextarea.value.substring(0, position - 1)
-    + virtTextarea.value.substring(position);
-    virtTextarea.focus();
-    virtTextarea.setSelectionRange(position - 1, position - 1);
+    inputText.value = inputText.value.substring(0, position - 1)
+    + inputText.value.substring(position);
+    inputText.focus();
+    inputText.setSelectionRange(position - 1, position - 1);
   }
 }
 
-// Функция нажатия на capslock в textarea
-const virtCapsLock = document.querySelector('#Key_Caps');
-function capslockClicked() { 
-  // Нужно учесть что при capslock, shift выполняют обратную функцию
-  if (shiftStatus === 0) shiftStatus = 1;
-  else shiftStatus = 0;
-  translate();
-  virtCapsLock.classList.toggle('active');
+function capslockClicked() {
+  if (shiftState === 0) shiftState = 1;
+  else shiftState = 0;
+  changeLetters();
+  btnCapsLock.classList.toggle('pressed');
+  btnCapsLock.classList.toggle('dark');
 }
 
-// Смена языка. Пусть первичное значение переменной - false, что действие по смене не было произведено
-let languagechange = false;
+let langswitched = false;
+
 function shiftClicked() {
-  if (languagechange) return; // для предотвращения многократного изменения языка
-  // если альт нажат, 
-  if (altStatus === 1) {
-    languagechange = true;
+  if (langswitched) return; // to prevent multiple case change
+  if (altState === 1) {
+    langswitched = true;
     switch (lang) {
       case 'en':
         lang = 'ru';
@@ -293,54 +237,45 @@ function shiftClicked() {
       default:
         break;
     }
-    setLanguage(lang);
+    setLang(lang);
   } else {
-    if (shiftStatus === 0) shiftStatus = 1;
-    else shiftStatus = 0;
-    translate();
+    if (shiftState === 0) shiftState = 1;
+    else shiftState = 0;
+    changeLetters();
   }
-}
-function shiftUp() {
-  if (languagechange) {
-    languagechange = false;
-    return;
-  }
-  if (shiftStatus === 0) shiftStatus = 1;
-  else shiftStatus = 0;
-  translate();
 }
 
-// Ctrl пуст т.к. не влияет на выводимый текст
+function shiftUp() {
+  if (langswitched) {
+    langswitched = false;
+    return;
+  }
+  if (shiftState === 0) shiftState = 1;
+  else shiftState = 0;
+  changeLetters();
+}
+
 function ctrlClicked() {
 
 }
 
-// Функция нажатия alt
 function altClicked() {
-  altStatus = 1;
+  altState = 1;
 }
 
-// Функция отжатого alt
 function altUp() {
-  altStatus = 0;
+  altState = 0;
 }
-
-
-//
-// Переходим к созданию переменных для клавиш с префиксом virt
-//
-
-
 
 function keyDown(event) {
   event.preventDefault();
   const k = findKey(event.code);
   if (k === null) return;
-  if (k.key !== 'CapsLock') document.querySelector(`#${event.code}`).classList.add('active');
-  if (k.property !== 'printable') {
+  if (k.key !== 'CapsLock') document.querySelector(`#${event.code}`).classList.add('pressed');
+  if (k.property === 'printable') {
     let s;
-    if (shiftStatus === 0) s = k.name;
-    else s = k.shiftName;
+    if (shiftState === 0) s = k.name;
+    else s = k.nameShift;
     insertSymbols(s);
   } else {
     switch (k.key) {
@@ -375,13 +310,11 @@ function keyDown(event) {
   }
 }
 
-
-
 function keyUp(event) {
   event.preventDefault();
   const k = findKey(event.code);
   if (k === null) return;
-  if (k.key !== 'CapsLock') document.querySelector(`#${event.code}`).classList.remove('active');
+  if (k.key !== 'CapsLock') document.querySelector(`#${event.code}`).classList.remove('pressed');
   if (k.property !== 'printable') {
     switch (k.key) {
       case 'Shift':
@@ -397,24 +330,24 @@ function keyUp(event) {
   }
 }
 
-virtTextarea.addEventListener('keydown', keyDown);
-virtTextarea.addEventListener('keyup', keyUp);
-virtKeyboard.addEventListener('keydown', keyDown);
-virtKeyboard.addEventListener('keyup', keyUp);
-virtEnter.addEventListener('click', enterClicked);
-virtTab.addEventListener('click', tabClicked);
-virtDel.addEventListener('click', delClicked);
-virtBackspace.addEventListener('click', backspaceClicked);
-virtCapsLock.addEventListener('click', capslockClicked);
-virtShiftL.addEventListener('mousedown', shiftClicked);
-virtShiftL.addEventListener('mouseup', shiftUp);
-virtShiftR.addEventListener('mousedown', shiftClicked);
-virtShiftR.addEventListener('mouseup', shiftUp);
-virtCtrlL.addEventListener('click', ctrlClicked);
-virtCtrlR.addEventListener('click', ctrlClicked);
-virtAltL.addEventListener('mousedown', altClicked);
-virtAltR.addEventListener('mousedown', altClicked);
-virtAltL.addEventListener('mouseup', altUp);
-virtAltR.addEventListener('mouseup', altUp);
-virtSpace.addEventListener('click', spaceClicked);
-virtSymbols.forEach((el) => el.addEventListener('click', btnsClicked));
+inputText.addEventListener('keydown', keyDown);
+inputText.addEventListener('keyup', keyUp);
+keyboard.addEventListener('keydown', keyDown);
+keyboard.addEventListener('keyup', keyUp);
+btnEnter.addEventListener('click', enterClicked);
+btnTab.addEventListener('click', tabClicked);
+btnDel.addEventListener('click', delClicked);
+btnBackspace.addEventListener('click', backspaceClicked);
+btnCapsLock.addEventListener('click', capslockClicked);
+btnShiftL.addEventListener('mousedown', shiftClicked);
+btnShiftL.addEventListener('mouseup', shiftUp);
+btnShiftR.addEventListener('mousedown', shiftClicked);
+btnShiftR.addEventListener('mouseup', shiftUp);
+btnCtrlL.addEventListener('click', ctrlClicked);
+btnCtrlR.addEventListener('click', ctrlClicked);
+btnAltL.addEventListener('mousedown', altClicked);
+btnAltR.addEventListener('mousedown', altClicked);
+btnAltL.addEventListener('mouseup', altUp);
+btnAltR.addEventListener('mouseup', altUp);
+btnSpace.addEventListener('click', spaceClicked);
+btnSymbols.forEach((el) => el.addEventListener('click', btnsClicked));
