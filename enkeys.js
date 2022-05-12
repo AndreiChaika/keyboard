be83ca197b6f78824436229374c320ebe00aa2ef
Andrei Chaika <redsn@inbox.ru>
2022-05-12 05:07:25 +0300

    fix: enkeys

--- enkeys.js
+++ enkeys.js
@@ -1,5 +1,5 @@
 const enLang = [
-    [{key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', keyCode: 17, code: 'ControlLeft', location: 1, altKey: true, ctrlKey: false, metaKey: true, shiftKey: true, description: 'ctrl', unicode: '^', class: 'size2 dark', property: 'nonprintable', }, {key: 'Meta', name: 'Win', nameShift: 'Win', keyCode: 91, code: 'MetaLeft', location: 1, altKey: false, ctrlKey: false, metaKey: true, shiftKey: false, description: 'Windows Key / Left ⌘ / Chromebook Search key', unicode: '⌘ ⊞', class: 'size2 dark', property: 'nonprintable', }, {key: 'Alt', name: 'Alt', nameShift: 'Alt', keyCode: 18, code: 'AltLeft', location: 1, altKey: true, ctrlKey: false, metaKey: true, shiftKey: false, description: 'Alt / Option', unicode: '⎇ / ⌥', class: 'size2 dark', property: 'nonprintable', }, ], 
+    [{key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', keyCode: 17, code: 'ControlLeft', location: 1, altKey: true, ctrlKey: false, metaKey: true, shiftKey: true, description: 'ctrl', unicode: '^', class: 'size2 dark', property: 'nonprintable', }, ], 
 ];
  
 
