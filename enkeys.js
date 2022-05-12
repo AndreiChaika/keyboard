202578fc0c2f733e46c0fd0c416bc4e2acdccf83
Andrei Chaika <redsn@inbox.ru>
2022-05-12 05:05:17 +0300

    fix: enkeys

--- enkeys.js
+++ enkeys.js
@@ -1,6 +1,7 @@
 const enLang = [
-    [{key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', code: 'ControlLeft', }, {key: 'Meta', name: 'Win', nameShift: 'Win', code: 'MetaLeft', }, {key: 'Alt', name: 'Alt', nameShift: 'Alt', code: 'AltLeft', }, {key: ' ', name: ' ', nameShift: ' ', code: 'Space', }, {key: 'Alt', name: 'Alt', nameShift: 'Alt', code: 'AltRight', }, {key: 'ArrowLeft', name: '◄', nameShift: '◄', code: 'ArrowLeft', }, {key: 'ArrowDown', name: '▼', nameShift: '▼', code: 'ArrowDown', }, {key: 'ArrowRight', name: '►', nameShift: '►', code: 'ArrowRight', }, {key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', code: 'ControlRight', }, ], 
-]; 
+    [{key: 'Control', name: 'Ctrl', nameShift: 'Ctrl', keyCode: 17, code: 'ControlLeft', location: 1, altKey: true, ctrlKey: false, metaKey: true, shiftKey: true, description: 'ctrl', unicode: '^', class: 'size2 dark', property: 'nonprintable', }, {key: 'Meta', name: 'Win', nameShift: 'Win', keyCode: 91, code: 'MetaLeft', location: 1, altKey: false, ctrlKey: false, metaKey: true, shiftKey: false, description: 'Windows Key / Left ⌘ / Chromebook Search key', unicode: '⌘ ⊞', class: 'size2 dark', property: 'nonprintable', }, {key: 'Alt', name: 'Alt', nameShift: 'Alt', keyCode: 18, code: 'AltLeft', location: 1, altKey: true, ctrlKey: false, metaKey: true, shiftKey: false, description: 'Alt / Option', unicode: '⎇ / ⌥', class: 'size2 dark', property: 'nonprintable', }, ], 
+];
+ 
 
     export default enLang;
   
